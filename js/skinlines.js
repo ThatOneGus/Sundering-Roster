c073f352f8fb7a30a529986343a6d9ca999ceb4b
Gustavo <149606961+ThatOneGus@users.noreply.github.com>
2026-09-04 23:18:29 -0400

    Create skinlines.js

---
+++ js/skinlines.js
@@ -0,0 +1,29 @@
+window.skinlines = {
+
+    "Drowned Horizon": {
+        name: "Drowned Horizon",
+
+        subtitle:
+            "Skinline",
+
+        description:
+            "A drowned world of abyssal oceans, drowned kingdoms, sea monsters, and cursed survivors.",
+
+        banner:
+            "Assets/Skinlines/Banner/IMG_6931.png"
+    },
+
+    "Overpaint": {
+        name: "Overpaint",
+
+        subtitle:
+            "Skinline",
+
+        description:
+            "A contemporary graffiti-covered universe where every character looks like they walked straight through wet spray paint and somehow made it fashion.",
+
+        banner:
+            "Assets/Skins/Skinlines/Overpaint/banner.png"
+    }
+
+};
