6707deb4f636f9f67870826dbcd0011638cd89e0
Gustavo <149606961+ThatOneGus@users.noreply.github.com>
2026-09-09 16:13:14 -0400

    Update skinlines.js

--- js/skinlines.js
+++ js/skinlines.js
@@ -1,7 +1,72 @@
 window.skinlines = {
 
+    "Recolors": {
+        name: "Recolors",
+
+        subtitle:
+            "Base Recolors",
+
+        description:
+            "A simple palette swap.",
+
+        banner:
+            "Assets/Skinlines/Banners/thumbnail.png"
+    },
+
+    "Broken Fairytale": {
+        name: "Broken Fairytale",
+
+        subtitle:
+            "Halloween Collection",
+
+        description:
+            "Not every story ends in Happily Ever After...",
+
+        banner:
+            "Assets/Skinlines/Banners/IMG_7071.png"
+    },
+
+    "Deadly Sins": {
+        name: "Deadly Sins",
+
+        subtitle:
+            "Skinline",
+
+        description:
+            "Hate the sin, love the sinner.",
+
+        banner:
+            "Assets/Skinlines/Banners/IMG_7067.png"
+    },
+
+    "Dreary Dollhouse": {
+        name: "Dreary Dollhouse",
+
+        subtitle:
+            "Halloween Collection",
+
+        description:
+            "Cute. Creepy. Cracked... Some dolls don't remain in the cradle.",
+
+        banner:
+            "Assets/Skinlines/Banners/IMG_7077.png"
+    },
+
     "Drowned Horizon": {
         name: "Drowned Horizon",
+
+        subtitle:
+            "Skinline",
+
+        description:
+            "Some treasures were never meant to be found.",
+
+        banner:
+            "Assets/Skinlines/Banners/IMG_7066.png"
+    },
+
+    "Elarian Banquet": {
+        name: "Elarian Banquet",
 
         subtitle:
             "Skinline",
@@ -10,7 +75,7 @@ window.skinlines = {
             "A drowned world of abyssal oceans, drowned kingdoms, sea monsters, and cursed survivors.",
 
         banner:
-            "Assets/Skinlines/Banners/IMG_6936.png"
+            "Assets/Skinlines/Banners/IMG_7072.png"
     },
 
     "Overpaint": {
@@ -23,7 +88,7 @@ window.skinlines = {
             "A contemporary graffiti-covered universe where every character looks like they walked straight through wet spray paint and somehow made it fashion.",
 
         banner:
-            "Assets/Skins/Skinlines/Overpaint/banner.png"
+            "Assets/Skins/Skinlines/Banners/IMG_7070.png"
     }
 
 };
